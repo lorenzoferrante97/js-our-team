4eaadb9c897d4afafcd2cb650bf682e49a86e0cb
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-23 14:33:29 +0100

    add: cards js to html

--- esercizio-js/script.js
+++ esercizio-js/script.js
@@ -36,3 +36,28 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+const cardContainer = document.getElementById("card-container");
+
+for (let i = 0; i < teamMembers.length; i++) {
+  console.log(i)
+  cardContainer.innerHTML += `
+            <!-- col -->
+            <div class="col-span-full sm:col-span-2 md:col-span-4">
+                <!-- card -->
+                <div class="flex flex-col lg:flex-row justify-between items-center rounded-md overflow-hidden p-4u max-lg:pb-7u lg:pr-7u bg-white border-1 border-smoke-100 gap-5u shadow-md shadow-smoke-100">
+                    <!-- img -->
+                    <figure class="w-full self-stretch max-lg:max-h-60u lg:w-[48%] overflow-hidden rounded">
+                        <img src="${teamMembers[i].img}" alt="${teamMembers[i].name}" class="img-responsive">
+                    </figure>
+                    <!-- card info -->
+                    <div class="flex flex-col gap-2u w-full">
+                        <p class="font-semibold text-2xl">${teamMembers[i].name}</p>
+                        <p class="px-3u py-u bg-blue-50 text-blue-500 rounded-full self-start text-sm">${teamMembers[i].role}</p>
+                        <a href="#" class="text-indigo-500 font-medium">${teamMembers[i].email}</a>
+                    </div>
+                </div>
+            </div>
+  `;
+
+}
